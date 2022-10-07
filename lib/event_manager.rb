999a3db3fb77413cbb459ec938b008eb4269ffb1
Olly Seber <olly_s@hotmail.co.uk>
2022-10-07 16:14:19 -0700

    Open file and split contents into rows and columns

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,1 +1,13 @@
 puts "Event Manager Initialized!"
+
+SAMPLE_FILE_PATH = "event_attendees.csv"
+
+abort unless File.exist?(SAMPLE_FILE_PATH)
+
+puts "Sample file identified"
+lines = File.readlines(SAMPLE_FILE_PATH)
+lines.each do |line|
+  columns = line.split(",")
+  print columns
+  puts
+end
