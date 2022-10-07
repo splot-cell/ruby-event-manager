af213500b9cf00f93e23ed85fe4100d9b4353926
Olly Seber <olly_s@hotmail.co.uk>
2022-10-07 16:35:04 -0700

    Parse, clean, and print zipcode data

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,5 +1,11 @@
 require "csv"
 
+def clean_zipcode(zipcode)
+  return "00000" if zipcode.nil?
+
+  zipcode.length == 5? zipcode : "00000#{zipcode}"[-5..-1]
+end
+
 puts "Event Manager Initialized!"
 
 SAMPLE_FILE_PATH = "event_attendees.csv"
@@ -9,5 +15,6 @@ abort unless File.exist?(SAMPLE_FILE_PATH)
 contents = CSV.open(SAMPLE_FILE_PATH, headers: true, header_converters: :symbol)
 contents.each do |row|
   name = row[:first_name]
-  puts name
+  zipcode = clean_zipcode(row[:zipcode])
+  puts "#{name}\t#{zipcode}"
 end
