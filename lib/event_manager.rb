d85a89c1e193c69f89986d3019c204bd0c8c951a
Olly Seber <olly_s@hotmail.co.uk>
2022-10-07 16:21:16 -0700

    Use header_converters to read csv file

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -6,8 +6,8 @@ SAMPLE_FILE_PATH = "event_attendees.csv"
 
 abort unless File.exist?(SAMPLE_FILE_PATH)
 
-contents = CSV.open(SAMPLE_FILE_PATH, headers: true)
+contents = CSV.open(SAMPLE_FILE_PATH, headers: true, header_converters: :symbol)
 contents.each do |row|
-  name = row[2]
+  name = row[:first_name]
   puts name
 end
