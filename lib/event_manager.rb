d000ff947dd6e7fbbc660f77bfa8d2a106bf9a7a
Olly Seber <olly_s@hotmail.co.uk>
2022-10-07 16:19:41 -0700

    Refactor to use csv library

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,14 +1,13 @@
+require "csv"
+
 puts "Event Manager Initialized!"
 
 SAMPLE_FILE_PATH = "event_attendees.csv"
 
 abort unless File.exist?(SAMPLE_FILE_PATH)
 
-puts "Sample file identified"
-lines = File.readlines(SAMPLE_FILE_PATH)
-lines.each_with_index do |line, index|
-  next if index == 0
-  columns = line.split(",")
-  name = columns[2]
+contents = CSV.open(SAMPLE_FILE_PATH, headers: true)
+contents.each do |row|
+  name = row[2]
   puts name
 end
