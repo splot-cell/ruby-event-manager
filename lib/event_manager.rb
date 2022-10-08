56685899a8bc06f7d4b37ef688332b2f2ebcf6e5
Olly Seber <olly_s@hotmail.co.uk>
2022-10-07 18:50:26 -0700

    Write personalized letters to html files

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -36,6 +36,7 @@ template_letter = File.read(TEMPLATE_LETTER_PATH)
 erb_template = ERB.new template_letter
 
 attendee_data.each do |row|
+  id = row[0]
   name = row[:first_name]
   zipcode = clean_zipcode(row[:zipcode])
 
@@ -43,5 +44,8 @@ attendee_data.each do |row|
 
   form_letter = erb_template.result(binding)
 
-  puts form_letter
+  Dir.mkdir("output") unless Dir.exist?("output")
+
+  filename = "output/thanks_#{id}.html"
+  File.open(filename, "w"){ |file| file.puts form_letter }
 end
