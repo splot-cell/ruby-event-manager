abcf5d7b83429228ae7fbd04710f7941ba4a8285
Olly Seber <olly_s@hotmail.co.uk>
2022-10-07 17:34:45 -0700

    Use Google API to print officials data per zipcode

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,4 +1,8 @@
 require "csv"
+require "google/apis/civicinfo_v2"
+
+civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
+civic_info.key = "AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw"
 
 def clean_zipcode(zipcode)
   zipcode.to_s.rjust(5, "0")[0..4]
@@ -8,11 +12,22 @@ puts "Event Manager Initialized!"
 
 SAMPLE_FILE_PATH = "event_attendees.csv"
 
-abort unless File.exist?(SAMPLE_FILE_PATH)
-
 contents = CSV.open(SAMPLE_FILE_PATH, headers: true, header_converters: :symbol)
 contents.each do |row|
   name = row[:first_name]
   zipcode = clean_zipcode(row[:zipcode])
-  puts "#{name}\t#{zipcode}"
+
+  begin
+    legisators = civic_info.representative_info_by_address(
+      address: zipcode,
+      levels: "country",
+      roles: ["legislatorUpperBody", "legislatorLowerBody"]
+    )
+    legisators = legisators.officials
+  rescue
+    "You can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials"
+  end
+
+
+  puts "#{name}\t#{zipcode}\t#{legisators}"
 end
