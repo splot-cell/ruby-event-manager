1f1a0bf71f0517de5e17f07c999b0e0219fa994b
Olly Seber <olly_s@hotmail.co.uk>
2022-10-07 17:45:36 -0700

    Refactor out legislators_from_zipcode method

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,21 +1,14 @@
 require "csv"
 require "google/apis/civicinfo_v2"
 
-civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
-civic_info.key = "AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw"
 
 def clean_zipcode(zipcode)
   zipcode.to_s.rjust(5, "0")[0..4]
 end
 
-puts "Event Manager Initialized!"
-
-SAMPLE_FILE_PATH = "event_attendees.csv"
-
-contents = CSV.open(SAMPLE_FILE_PATH, headers: true, header_converters: :symbol)
-contents.each do |row|
-  name = row[:first_name]
-  zipcode = clean_zipcode(row[:zipcode])
+def legislators_by_zipcode(zipcode)
+  civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
+  civic_info.key = "AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw"
 
   begin
     legislators = civic_info.representative_info_by_address(
@@ -24,10 +17,22 @@ contents.each do |row|
       roles: ["legislatorUpperBody", "legislatorLowerBody"]
     )
     legislators = legislators.officials
-    legislator_names = legislators.map(&:name)
+    legislator_names = legislators.map(&:name).join(", ")
   rescue
     "You can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials"
   end
+end
 
-  puts "#{name}\t#{zipcode}\t#{legislator_names}"
+puts "Event Manager Initialized."
+
+SAMPLE_FILE_PATH = "event_attendees.csv"
+
+contents = CSV.open(SAMPLE_FILE_PATH, headers: true, header_converters: :symbol)
+contents.each do |row|
+  name = row[:first_name]
+  zipcode = clean_zipcode(row[:zipcode])
+
+  legislators = legislators_by_zipcode(zipcode)
+
+  puts "#{name}\t#{zipcode}\t#{legislators}"
 end
